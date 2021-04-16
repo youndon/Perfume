aea890d35e717ada7cd142002c716e98eb4a3c74
Yon-done <bourntivoli@gmail.com>
2021-04-16 02:36:20 +0100

    16/04/2021

--- src/main/kotlin/com/example/demo/MainLand.kt
+++ src/main/kotlin/com/example/demo/MainLand.kt
@@ -1,15 +1,9 @@
 package com.example.demo.view
 
-import javafx.beans.property.SimpleObjectProperty
-import javafx.collections.FXCollections
-import javafx.geometry.Orientation
-import javafx.scene.paint.Color
-import org.nield.dirtyfx.beans.DirtyObjectProperty
-import org.nield.dirtyfx.beans.DirtyStringProperty
-import org.nield.dirtyfx.extensions.addTo
-import org.nield.dirtyfx.tracking.CompositeDirtyProperty
-import tornadofx.*
-import java.time.LocalDate
+import tornadofx.App
+import tornadofx.UIComponent
+import tornadofx.launch
+import tornadofx.vbox
 
 fun main() {
     launch<MainLand>()
